60fb5ffc9dd5910b6ceaac65e607559bafa813bb
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2025-02-06 16:51:04 -0700

    Added a shift counter for the LEDs and a blank encrypt module

---
+++ encrypt.vhd
@@ -0,0 +1,12 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.display_types.ALL;
+ENTITY encrypt IS
+
+END encrypt;
+
+ARCHITECTURE behavior OF encrypt IS
+BEGIN
+
+END behavior;
