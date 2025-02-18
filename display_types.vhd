9092853b037c4086dcc814f511587f64f8c5a972
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2025-02-18 13:44:47 -0700

    fix: Resolve VHDL element count mismatch and enable 8-bit pattern control

--- display_types.vhd
+++ display_types.vhd
@@ -2,5 +2,5 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 PACKAGE display_types IS
-	TYPE display_array_t IS ARRAY (0 TO 5) OF STD_LOGIC_VECTOR(6 DOWNTO 0);
+	TYPE display_array_t IS ARRAY (0 TO 5) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
 END PACKAGE;
